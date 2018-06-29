82e8ee5f485d11383848b337803109f6860ba759
cagryinside <cagricetin89@gmail.com>
2018-06-29 17:17:04 -0400

    add group by search

--- src/main/java/usf/edu/bronie/sqlcrawler/constants/RegexConstants.java
+++ src/main/java/usf/edu/bronie/sqlcrawler/constants/RegexConstants.java
@@ -4,7 +4,7 @@ public class RegexConstants {
 
     public static final String STRING_LITERAL_CONCAT_WITH_VAR = "\\\"[^\\\"\\\\]*(\\\\.[^\\\"\\\\]*)*\\\"(?=( )*(\\r\\n\\|\\r\\|\\n)*( )*\\+( )*(\\r\\n\\|\\r\\|\\n)*( )*[_a-zA-Z][_a-zA-Z0-9]*)";
 
-    public static final String STRING_LITERAL_CONCAT_WITH_ORDER_BY = "\\\"[^\\\"\\\\]*(\\\\.[^\\\"\\\\]*)*ORDER BY(\\r\\n|\\r|\\n|\\t| )*\\\"(?=(\\r\\n|\\r|\\n|\\t| )*\\+(\\r\\n|\\r|\\n|\\t| )*[_a-zA-Z][_a-zA-Z0-9]*)";
+    public static final String STRING_LITERAL_CONCAT_WITH_ORDER_BY = "\\\"[^\\\"\\\\]*(\\\\.[^\\\"\\\\]*)*GROUP BY(\\r\\n|\\r|\\n|\\t| )*\\\"(?=(\\r\\n|\\r|\\n|\\t| )*\\+(\\r\\n|\\r|\\n|\\t| )*[_a-zA-Z][_a-zA-Z0-9]*)";
 
     public static final String STRING_LITERAL = "\\\"[^\\\"\\\\]*(\\\\.[^\\\"\\\\]*)*\\\"";
 
@@ -13,6 +13,8 @@ public class RegexConstants {
     public static final String[] SQL_KEYWORDS = {"select", "update", "delete", "insert into",
             "create database", "alter database", "create table", "alter table", "drop table",
             "create index", "drop index", "order by", "group by", "desc", "asc", "join", "where"};
+
+    public static final String SPECIFIC_KEYWORD = "group by";
 
     public static final String PREPARED_STATEMENT_KEYWORD = "?";
 
--- src/main/java/usf/edu/bronie/sqlcrawler/constants/UrlConstants.java
+++ src/main/java/usf/edu/bronie/sqlcrawler/constants/UrlConstants.java
@@ -2,7 +2,7 @@ package usf.edu.bronie.sqlcrawler.constants;
 
 public class UrlConstants {
 
-    public static final String SEARCHCODE_URL = "https://searchcode.com/api/codesearch_I/?q=executeQuery+order+by&lan=23&per_page=100&p=";
+    public static final String SEARCHCODE_URL = "https://searchcode.com/api/codesearch_I/?q=executeQuery+group+by&lan=23&per_page=100&p=";
 
     public static final int SEARCHCODE_MAX_PAGE = 50;
 }
