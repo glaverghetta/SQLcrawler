7291aa60e7aa8e66a1dce6e579eda8dd67901b05
Kevin Dennis <kevin.dennis1995@yahoo.com>
2022-09-26 08:16:52 -0400

    Cached regex analyzer patterns and fixed regx bug

--- src/main/java/usf/edu/bronie/sqlcrawler/analyze/TableNameCodeAnalyzer.java
+++ src/main/java/usf/edu/bronie/sqlcrawler/analyze/TableNameCodeAnalyzer.java
@@ -1,25 +1,21 @@
 package usf.edu.bronie.sqlcrawler.analyze;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import usf.edu.bronie.sqlcrawler.constants.RegexConstants;
-import usf.edu.bronie.sqlcrawler.manager.CodeAnalysisManager;
 import usf.edu.bronie.sqlcrawler.model.SQLType;
 import usf.edu.bronie.sqlcrawler.utils.RegexUtils;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class TableNameCodeAnalyzer implements CodeAnalyzer {
 
-	private String mStringLitPattern = RegexConstants.TABLE + RegexConstants.CONCAT_VAR;
-	 private String mStringLitPatternMultiple = RegexConstants.TABLE + RegexConstants.CONCAT_VAR_MULTIPLE;
-	/*
-    private Pattern mStringLitWithFromIntoPattern = Pattern.compile(RegexConstants.TABLE + RegexConstants.CONCAT_VAR,
-            Pattern.CASE_INSENSITIVE);
+    private String mStringLitPattern = RegexConstants.TABLE + RegexConstants.CONCAT_VAR;
+    private String mStringLitPatternMultiple = RegexConstants.TABLE + RegexConstants.CONCAT_VAR_MULTIPLE;
 
-    private Pattern mStringLitWithFromIntoPatternMultiple = Pattern.compile(RegexConstants.TABLE + RegexConstants.CONCAT_VAR_MULTIPLE,
-            Pattern.CASE_INSENSITIVE);
-    */
+    RegexConstants.Languages lastUsedLang = null;
+    Pattern stringLiteralPatternJava;
+    Pattern stringLitPatternMultipleJava;
+    
     private static final String DBFIELD = "table_usage";
 
     public String getDBField() {
@@ -34,20 +30,23 @@ public class TableNameCodeAnalyzer implements CodeAnalyzer {
         String variable = RegexConstants.getVariable(language);
         String concat = RegexConstants.getConcat(language);
 
-        Pattern stringLiteralPatternJava = Pattern.compile(String.format(mStringLitPattern,
-        		concat,
-				variable),
-	            Pattern.CASE_INSENSITIVE);
-
-	    Pattern stringLitPatternMultipleJava = Pattern.compile(String.format(mStringLitPatternMultiple,
-	    		concat,
-				variable),
-	            Pattern.CASE_INSENSITIVE);
-	    
-        return RegexUtils.isConcat(code, stringLiteralPatternJava) || RegexUtils.isConcat(code, stringLitPatternMultipleJava) ?
-        		SQLType.STRING_CONCAT : SQLType.HARDCODED;
-    }	
+        if(language != lastUsedLang){
+            lastUsedLang = language;
+            stringLiteralPatternJava = Pattern.compile(String.format(mStringLitPattern,
+                    concat,
+                    variable),
+                    Pattern.CASE_INSENSITIVE);
     
+            stringLitPatternMultipleJava = Pattern.compile(String.format(mStringLitPatternMultiple,
+                    variable,
+                    concat,
+                    variable),
+                    Pattern.CASE_INSENSITIVE);
+        }
+
+        return RegexUtils.isConcat(code, stringLiteralPatternJava)
+                || RegexUtils.isConcat(code, stringLitPatternMultipleJava) ? SQLType.STRING_CONCAT : SQLType.HARDCODED;
+    }
 
     public SQLType analyzeCode(String code) {
         return null;
