6e0aa05728883c3b13e92f0ad98befa1fbded5d1
Kevin Dennis <kevin.dennis1995@yahoo.com>
2023-02-20 18:09:43 -0500

    Email resume

--- resume.py
+++ resume.py
@@ -1,7 +1,45 @@
 import glob
 import subprocess
+import imaplib
+import traceback
+import yagmail
+import keyring
+import email
+import time
+import datetime
 
-CMD = ["java", "-jar", "target/sqlcrawler-1.0-jar-with-dependencies.jar"]
+MYEMAIL = "acdcprovenance@gmail.com"
+MYNAME = "Kevin"
+
+def sendEmail(subject, content):
+    yagmail.SMTP('acdcprovenance').send(MYEMAIL, subject, content)
+
+def check_for_reply_from_self(subject):
+    try:
+        mail = imaplib.IMAP4_SSL("imap.gmail.com" )
+        mail.login(MYEMAIL, keyring.get_password("yagmail", MYEMAIL))
+        mail.select('inbox', readonly=True)
+
+        data = mail.search(None, 'FROM', MYEMAIL)
+        mail_ids = data[1]
+        id_list = mail_ids[0].split()   
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+
+        for i in range(latest_email_id,first_email_id, -1):
+            data = mail.fetch(str(i), '(RFC822)' )
+            for response_part in data:
+                arr = response_part[0]
+                if isinstance(arr, tuple):
+                    msg = email.message_from_string(str(arr[1],'utf-8'))
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    if "Re:" in email_subject and subject in email_subject:
+                        return True
+        return False
+    except Exception as e:
+        traceback.print_exc() 
+        print(str(e))
 
 def findArguments(filename):
     with open(filename) as file:
@@ -30,7 +68,7 @@ def findDetails(filename):
                 frameEnd = frameInfo[1]
     return page, frameStart, frameEnd
 
-if __name__ == '__main__':
+def resume():
     filename = glob.glob("logs/Final*")[-1]
     print(f"Most recent Final file {filename}")
     args = findArguments(filename)
@@ -45,7 +83,7 @@ if __name__ == '__main__':
 
     page, start, end = findDetails(filename)
 
-    newArgs = CMD
+    newArgs = ["java", "-jar", "target/sqlcrawler-1.0-jar-with-dependencies.jar"]
     ignore = False
     for i in args:
         if ignore:
@@ -66,8 +104,41 @@ if __name__ == '__main__':
 
     print(f"Restarting with frame {start}-{end}, page {page}")
     print(f"Running the following command: ", ' '.join(newArgs))
-    print("But first... rebuild!")
-    mvn = subprocess.run(["mvn", "package"], shell=True, stderr=subprocess.STDOUT)
-    if mvn.returncode == 0:
-        subprocess.run(newArgs, shell=True, stderr=subprocess.STDOUT)
+
+    lastLines = []
+    with subprocess.Popen(newArgs, shell=True, stdout=subprocess.PIPE, bufsize=1,
+           universal_newlines=True, stderr=subprocess.STDOUT, encoding='utf-8') as p:
+        for line in p.stdout:
+            print(line, end='')
+            lastLines.append(line)
+            if len(lastLines) > 100:
+                lastLines.pop(0)
+    return lastLines
+
+if __name__ == '__main__':
+    print("First... rebuild!")
+    mvn = subprocess.run(["mvn", "package"], shell=True, stderr=subprocess.STDOUT, encoding='utf-8')
+    if mvn.returncode != 0:
+        print("Failed to build!")
+        exit(-1)
+
+    while True:
+        lastLines = resume()
+        print("Crashed? Wait a minute, then email the boss")
+        start = datetime.datetime.now()
+        time.sleep(60)  # Wait a minute in case we crashed due to network instability
+        fail = True
+        subject = MYNAME + " " + str(start)
+        sendEmail(subject, ''.join(lastLines))
+        print(f"Sent last 100 lines to myself with subject {start}")
+        while((datetime.datetime.now() - start).days == 0):
+            if(check_for_reply_from_self(subject)):
+                fail = False
+                break
+            print(f"No email yet at {datetime.datetime.now()}. Check again in 5.")
+            time.sleep(60*5) # Check email again in 5 minutes
+        if fail:
+            exit(-1)
+    
+        
         
